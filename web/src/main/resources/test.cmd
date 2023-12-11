56bc90c121e86003110202bef8f5783c18bf29c2
thachln <thachln@gmail.com>
2023-12-11 17:11:26 +0700

    Fix filename "auto..." to "audio2text.py"

--- web/src/main/resources/test.cmd
+++ web/src/main/resources/test.cmd
@@ -1,8 +1,9 @@
 @ECHO OFF
 ECHO Uncomment below command in the first time to install libraries:
 ECHO pip install SpeechRecognition pydub
+ECHO .
 
 REM pip install SpeechRecognition pydub
 ECHO Testing python script with audio "This audio program has been produced by ybm.All rights reserved."
-python auto2text.py 1_Intro.mp3
+python audio2text.py 1_Intro.mp3
 @PAUSE
